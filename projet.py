8485cb2073ae817738e3323d27e8aa59985fbda6
MariemZainine <zaininemariem@gmail.com>
2025-03-11 11:14:55 +0100

    dans un seule commit squash

--- projet.py
+++ projet.py
@@ -5,6 +5,18 @@ for t in T
 S+=t
 return S
 Data = [1,3,5]
+<<<<<<< HEAD
 Som= sum(Data)
 print("La somme est :",Som)
+
+if Data:
+
+print ("la somme est:"sum(Data))
+
+print ("le min est:"min(Data))
+
+print ("le max est:"max(Data))
+ 
+else:
+print("liste vide ")
 
